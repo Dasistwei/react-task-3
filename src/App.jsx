3a8ee346831fa8f677628052c2269e2e5b99a462
wei <wei02919@gmail.com>
2025-03-01 22:28:25 +0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,35 @@
-import { useState } from "react";
-
-function App() {
+import { useEffect, useState } from "react";
+import axios from "axios";
+function ProductPage({ token }) {
   const [tempProduct, setTempProduct] = useState(null);
-  const [products, setProducts] = useState([
-    {
-      category: "甜甜圈",
-      content: "尺寸：14x14cm",
-      description:
-        "濃郁的草莓風味，中心填入滑順不膩口的卡士達內餡，帶來滿滿幸福感！",
-      id: "-L9tH8jxVb2Ka_DYPwng",
-      is_enabled: 1,
-      origin_price: 150,
-      price: 99,
-      title: "草莓莓果夾心圈",
-      unit: "元",
-      num: 10,
-      imageUrl: "https://images.unsplash.com/photo-1583182332473-b31ba08929c8",
-      imagesUrl: [
-        "https://images.unsplash.com/photo-1626094309830-abbb0c99da4a",
-        "https://images.unsplash.com/photo-1559656914-a30970c1affd",
-      ],
-    },
-    {
-      category: "蛋糕",
-      content: "尺寸：6寸",
-      description:
-        "蜜蜂蜜蛋糕，夾層夾上酸酸甜甜的檸檬餡，清爽可口的滋味讓人口水直流！",
-      id: "-McJ-VvcwfN1_Ye_NtVA",
-      is_enabled: 1,
-      origin_price: 1000,
-      price: 900,
-      title: "蜂蜜檸檬蛋糕",
-      unit: "個",
-      num: 1,
-      imageUrl:
-        "https://images.unsplash.com/photo-1627834377411-8da5f4f09de8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80",
-      imagesUrl: [
-        "https://images.unsplash.com/photo-1618888007540-2bdead974bbb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=987&q=80",
-      ],
-    },
-    {
-      category: "蛋糕",
-      content: "尺寸：6寸",
-      description: "法式煎薄餅加上濃郁可可醬，呈現經典的美味及口感。",
-      id: "-McJ-VyqaFlLzUMmpPpm",
-      is_enabled: 1,
-      origin_price: 700,
-      price: 600,
-      title: "暗黑千層",
-      unit: "個",
-      num: 15,
-      imageUrl:
-        "https://images.unsplash.com/photo-1505253149613-112d21d9f6a9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fGNha2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60",
-      imagesUrl: [
-        "https://images.unsplash.com/flagged/photo-1557234985-425e10c9d7f1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA5fHxjYWtlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60",
-        "https://images.unsplash.com/photo-1540337706094-da10342c93d8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDR8fGNha2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60",
-      ],
-    },
-  ]);
+  const [products, setProducts] = useState([]);
+  // console.log(token);
+  function getProducts() {
+    const API_URL =
+      "https://ec-course-api.hexschool.io/v2/api/wei777/admin/products";
+    const TOKEN = "eyJhbGciOiJSUzI1NiIsImtpZCI6IlFEZ1JhQSJ9...（省略）...";
 
+    axios
+      .get(API_URL, {
+        headers: {
+          Accept: "application/json",
+          Authorization: token,
+        },
+      })
+      .then((response) => {
+        console.log("✅ 產品列表：", response.data.products);
+        setProducts(response.data.products);
+      })
+      .catch((error) => {
+        console.error(
+          "❌ 請求錯誤:",
+          error.response ? error.response.data : error.message
+        );
+      });
+  }
+  useEffect(() => {
+    getProducts();
+  }, []);
   return (
     <div className="container">
       <div className="row mt-5">
@@ -80,7 +51,7 @@ function App() {
                   <td>{item.title}</td>
                   <td>{item.origin_price}</td>
                   <td>{item.price}</td>
-                  <td>{item.is_enabled}</td>
+                  <td>{item.is_enabled ? "啟用" : "不啟用"}</td>
 
                   <td>
                     <button
@@ -143,6 +114,90 @@ function App() {
         </div>
       </div>
     </div>
+  );
+}
+function App() {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+  const [isLogin, setIsLogin] = useState(false);
+  const [token, setToken] = useState("");
+
+  function login() {
+    axios
+      .post("https://ec-course-api.hexschool.io/v2/admin/signin", {
+        username: user.username,
+        password: user.password,
+      })
+      .then((response) => {
+        console.log(response.data);
+        setIsLogin(true);
+        setToken(response.data.token);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  function handleClick(e) {
+    e.preventDefault();
+    login();
+  }
+  return (
+    <>
+      {isLogin ? (
+        <ProductPage token={token} />
+      ) : (
+        <div className="container">
+          <form>
+            <fieldset>
+              <legend>登入</legend>
+              <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  onChange={(e) => {
+                    setUser({
+                      ...user,
+                      username: e.target.value,
+                    });
+                  }}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  onChange={(e) => {
+                    setUser({
+                      ...user,
+                      password: e.target.value,
+                    });
+                  }}
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn btn-primary"
+                onClick={handleClick}
+              >
+                Submit
+              </button>
+            </fieldset>
+          </form>
+        </div>
+      )}
+    </>
   );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/react-task-1/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/react-task-2/' : '/',
   plugins: [react()],
 })
